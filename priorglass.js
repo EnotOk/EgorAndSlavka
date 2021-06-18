b68f6c6dce9597373ae1d6beb1a79933d6886b35
slavkataras <starasenko2002@gmail.com>
2021-06-18 16:03:00 +0300

    Update priorglass.js

--- priorglass.js
+++ priorglass.js
@@ -47,7 +47,7 @@ function raschitat() {
     alert("Вы не указали длину");
     } else {
 
-    volume = parseFloat (shirina)* parseFloat (dlina)* parseFloat(tolshina);
+    // volume = parseFloat (shirina)* parseFloat (dlina)* parseFloat(tolshina);
 
     ploschad = parseFloat (shirina)* parseFloat (dlina);
     document.getElementById('ploschad').innerHTML = "Площадь равна: "+ ploschad +" кв. м.";
@@ -57,3 +57,68 @@ function raschitat() {
 
     }
     }
+ 
+function poschitat() {
+    tolshina = document.getElementById('tolshina').value;
+    switch (tolshina) {
+        case "3mm":
+         cena = 120;
+         break
+       case "4mm":
+         cena = 140;
+         break   
+       case "5mm":
+         cena = 160;
+         break   
+
+       case "6mm":
+           cena = 180;
+           break
+       
+       case "8mm":
+           cena = 220;
+           break
+       
+       case "10mm":
+           cena = 260;
+           break
+
+       case "12mm":
+           cena = 300;
+           break
+
+       case "15mm":
+           cena = 340;
+           break
+
+       case "19mm":
+           cena = 380;
+           break
+
+      default :
+         cena = 100;
+         break
+   }
+
+   shirina  = document.getElementById('shirina').value;
+   dlina  = document.getElementById('dlina').value;
+   tolshina  = document.getElementById('tolshina').value;
+   if(shirina == ""){
+   alert("Вы не указали ширину");
+   } else if(dlina == ""){
+   alert("Вы не указали длину");
+   } else if(tolshina == ""){
+    alert("Вы не указали толщину");
+   } else {
+
+   // volume = parseFloat (shirina)* parseFloat (dlina)* parseFloat(tolshina);
+
+   ploschad = parseFloat (shirina)* parseFloat (dlina);
+   document.getElementById('ploschad').innerHTML = "Площадь равна: "+ ploschad +" кв. м.";
+   
+   stoimost = ploschad*cena;
+   document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
+
+   }
+
+}
