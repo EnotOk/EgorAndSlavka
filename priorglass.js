f1ee8240f01d212ef028aad383c6120153448026
slavkataras <starasenko2002@gmail.com>
2021-06-18 15:48:20 +0300

    Update priorglass.js

--- priorglass.js
+++ priorglass.js
@@ -1,16 +1,41 @@
 function raschitat() {
-    cvet  = document.getElementById('cvet').value;
-    switch (cvet) {
-       case "serii":
+    stekla  = document.getElementById('stekla').value;
+    switch (stekla) {
+         case "zerkalo":
           cena = 440;
           break
-       case "goluboi":
+        case "prozrachnoe":
           cena = 480;
           break   
-        case "geltii":
+        case "osvetlennoe":
           cena = 380;
           break   
-       default:
+
+        case "zakalennoe":
+            cena = 400;
+            break
+        
+        case "triplex":
+            cena = 410;
+            break
+        
+        case "armirovannoe":
+            cena = 390;
+            break
+
+        case "stemalit":
+            cena = 500;
+            break
+
+        case "steklopaket":
+            cena = 470;
+            break
+
+        case "tonirovannoe":
+            cena = 460;
+            break
+
+       default :
           cena = 440;
           break
     }
@@ -21,9 +46,14 @@ function raschitat() {
     } else if(dlina == ""){
     alert("Вы не указали длину");
     } else {
+
+    volume = parseFloat (shirina)* parseFloat (dlina)* parseFloat(tolshina);
+
     ploschad = parseFloat (shirina)* parseFloat (dlina);
     document.getElementById('ploschad').innerHTML = "Площадь равна: "+ ploschad +" кв. м.";
+    
     stoimost = ploschad*cena;
     document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
+
     }
     }
